928f3f148bd414b4c4acbbdfe68bb8a2913aa71a
Chanchal Prajapati <chanchal13030@iiitd.ac.in>
2024-07-01 16:07:00 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+<script>
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Thank you for your message!');
+        // Optionally, you can submit the form here using AJAX
+    });
+</script>
